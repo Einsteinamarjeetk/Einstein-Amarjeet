e6b9ccd3e9da4e1c3d2b8f69635907a56bf4f966
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-05-18 17:16:23 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = 
-api_hash = ""
-bot_token = "6930088546:AAFOkh1wlahXpi-gPaN2GozXjSLo_nSHSYg"
+api_id = "26927668"
+api_hash = "fbc4fcff2ed1cbdafdedbf2c56e8b5d3"
+bot_token = "6548709662:AAF203SQCe6S_n2gW0bh9hI3ag3t9Z99Djs"
